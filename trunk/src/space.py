fc3c0a3d6496b1bf5ee78491c8411e569d7b98b7
manuelaraoz <manuelaraoz@052bd30f-83a9-422a-3aa1-39f78dec3271>
2010-09-01 04:56:39 +0000

    advancements in filters

--- trunk/src/space.py
+++ trunk/src/space.py
@@ -6,6 +6,7 @@ from math import log
 from random import random
 
 from util import draw_histogram, rand_exponential, rand_rayleigh, rand_gaussian
+from util import Matrix, EmptyMatrix
     
 
 
@@ -40,22 +41,55 @@ class SpaceBMPImage(PointBMPImage):
         self.normalize()
         return self
 
+    def linear_mask(self, mask):
+        m, n = len(mask), len(mask[0])
+        if m % 2 != 1 or n % 2 != 1:
+            raise ValueError("Mask must have odd number of rows and columns")
+        a, b = (m-1)/2, (n-1)/2
+        
+        copy = self.copy()
+        
+        for x in xrange(self.width):
+            for y in xrange(self.height):
+                for color in RGB_COLORS:
+                    total = 0
+                    for s in xrange(-a,a+1):
+                        for t in xrange(-b, b+1):
+                            value = self.get_pixel(x+s, y+t, color)
+                            weight = mask[s][t]
+                            total += value * weight
+                    copy.set_pixel(x, y, color, total)
+        return copy
+    
+    def mean_mask(self, order):
+        n = (2*order+1)
+        m = [[1.0/(n*n) for col in xrange(n)] for row in xrange(n)]
+        return self.linear_mask(m)
+
 if __name__ == "__main__":
     
     # load image from file
     megan = SpaceBMPImage("images/MEGAN.BMP")
+    mean1 = megan.mean_mask(5)
+    mean1.save("mean.bmp")
+    mean1.draw()
+    exit(0)
+    # add gaussian noise
     gaussian = megan.copy().add_gaussian_noise(15)
     gaussian.draw()
     gaussian.save("gaussian.bmp")
     
+    # add rayleigh noise
     rayleigh = megan.copy().add_rayleigh_noise(0.001)
     rayleigh.draw()
     rayleigh.save("rayleigh.bmp")
     
+    # add exponential noise
     exponential = megan.copy().add_exponential_noise(1)
     exponential.draw()
     exponential.save("exponential.bmp")
     
+    # add sant and pepper noise
     snp = megan.black_and_white().add_salt_and_pepper_noise()
     snp.draw()
     snp.save("salt&pepper.bmp")
