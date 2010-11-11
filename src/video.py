e353bce2a34b6b7c7ae0c2ffab060c16c5f62ea3
manuelaraoz <manuelaraoz@052bd30f-83a9-422a-3aa1-39f78dec3271>
2010-11-11 20:15:27 +0000

    video start

---
+++ src/video.py
@@ -0,0 +1,8 @@
+# *-* coding: utf-8 *-*
+
+from base import RGB_COLORS, RED, GREEN, BLUE
+from border import BorderBMPImage, L
+from math import log, fabs, atan2, pi, degrees, radians, sqrt, cos, sin
+from random import random
+
+
